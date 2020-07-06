43c9b3c8936ebc75e34fbf328da249cdad5a0154
Joseph cohen <jojocohen57@gmail.com>
2020-07-06 21:28:57 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 def speak_to_grandma(phrase)
   if phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
-  elsif phrase == phrase.upcase
+  elsif phrase != phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
 end  
 # Whatever you say to grandma, she should respond with
