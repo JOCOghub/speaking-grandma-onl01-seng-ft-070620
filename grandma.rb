3f9b5fd3542843ec7d8f736f77edccc0fec9582f
Joseph cohen <jojocohen57@gmail.com>
2020-07-06 21:28:43 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
-  if phrase != phrase.upcase
-    return "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == phrase.upcase
+  if phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
+  elsif phrase == phrase.upcase
+    return "HUH?! SPEAK UP, SONNY!"
 end  
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
